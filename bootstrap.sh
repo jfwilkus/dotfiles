7db2f624bd2ed9e66957407fc37f507a4722c0f1
James F Wilkus <jfwilkus@mac.com>
2014-02-02 08:19:40 -0500

    initial bootstrap script for new systems

---
+++ bootstrap.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+brew install wget
+brew install gnupg
+brew install git
+brew install rbenv
+brew install ruby-build
+
+brew tap homebrew/binary
+
+brew install packer
+
+brew tap phinze/cask
+
+brew install brew-cask
+
+brew cask install alfred
+brew cask alfred link
+
+brew cask install vagrant
+
+brew cask install firefox
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install harvest
+brew cask install evernote
+brew cask install kindle
+brew cask install rdio
+
+curl -L http://install.perlbrew.pl | bash
+
+source ~/.bashrc
+
+perlbrew install perl-5.18.2
+
+rbenv install 2.1.0
+
